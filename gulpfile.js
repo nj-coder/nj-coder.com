1171a7c5785c9abb1391f1d8001efd0d00b8908f
Nidhin Joseph <nidhin.joseph1@gmail.com>
2019-09-08 10:48:24 +1000

    added typewriter.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
 
 // deletes the dist folder
 gulp.task('clean', require('del').bind(null, ['dist']));
@@ -19,6 +20,16 @@ gulp.task('styles', function () {
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest('dist'));
 });
+// build js
+gulp.task('scripts', function () {
+    return gulp.src(['./src/**/*.js', './src/**/**/*.js'])
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('script.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+// build html
 gulp.task('html', function () {
     return gulp.src(['./src/**/*.html'])
         .pipe(gulp.dest('dist'));
@@ -27,8 +38,8 @@ gulp.task('html', function () {
 // Create a new build in dist folder
 gulp.task('build', gulp.series(
     'clean',
-    gulp.parallel('styles', 'html')
+    gulp.parallel('styles', 'html', 'scripts')
 ));
 
 // Watch for changes.
-gulp.task('watch', () => gulp.watch(['./src/*/*.scss', './src/**/*.html'], gulp.parallel('styles', 'html')));
+gulp.task('watch', () => gulp.watch(['./src/*.scss', './src/*/*.scss', './src/**/*.html'], gulp.parallel('styles', 'html')));
