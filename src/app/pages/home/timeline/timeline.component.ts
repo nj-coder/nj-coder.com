2c1b10d401fc1c503515b4d04bf71567a8e04c8a
Nidhin Joseph <nidhin.joseph1@gmail.com>
2019-10-27 13:40:27 +1100

    added lazy script loading service

--- src/app/pages/home/timeline/timeline.component.ts
+++ src/app/pages/home/timeline/timeline.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timeline.component.scss']
 })
 export class TimelineComponent implements OnInit {
+  timelineItems = [{
 
+  }];
   constructor() { }
 
   ngOnInit() {
--- src/app/pages/nj-timepicker/nj-timepicker.component.ts
+++ src/app/pages/nj-timepicker/nj-timepicker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LazyLoadingServiceService } from 'src/app/services/LazyLoadingService/lazy-loading-service.service';
 
 @Component({
   selector: 'app-nj-timepicker',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NjTimepickerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private lazyLoadService: LazyLoadingServiceService) { }
 
   ngOnInit() {
+    /* Usage */
+    this.lazyLoadService.loadScript('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js').subscribe(() => {
+      // code
+    });
   }
 
 }
