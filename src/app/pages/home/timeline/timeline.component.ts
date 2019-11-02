48e6dabdcd4fa2e232c7c0f7f86daf9bb82f2b05
Nidhin Joseph <nidhin.joseph1@gmail.com>
2019-11-02 17:08:20 +1100

    updated timeline

--- src/app/pages/home/timeline/timeline.component.ts
+++ src/app/pages/home/timeline/timeline.component.ts
@@ -9,18 +9,27 @@ export class TimelineComponent implements OnInit {
 
   timelineItems = [{
     type: 'position',
+    startDate: '01-07-2017',
+    endDate: 'current',
+    description: `Design, develop and test mobile and web-based applications for best user experience and performance.`,
     company: {
       name: '152HQ',
       link: 'https://www.152hq.com/wp/',
       title: 'Senior Front-end Developer'
     },
     techs: ['Angular', 'Ionic', 'AngularJS', 'Cordova'],
-    startDate: '07-07-2017',
-    endDate: 'current',
-    itemImage: ''
+    itemImage: 'assets/images/152hq.png'
   }, {
     type: 'project',
-    subtype: 'opensource'
+    subtype: 'opensource',
+    startDate: '01-08-2019',
+    description: `A simple and user-friendly timepicker using pure JavaScript`,
+    project: {
+      title: 'NJ Timepicker',
+      link: ''
+    },
+    techs: ['JavaScript', 'SASS', 'Gulp'],
+    itemImage: 'assets/images/github.png'
   }, {
     type: 'education'
   }];
